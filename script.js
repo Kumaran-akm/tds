bed4ea499a2de8a0dfc6fd704601018b32eaf118
Kumaran-akm <23ds3000049@ds.study.iitm.ac.in>
2024-10-07 21:56:40 +0530

    script.js

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function flipCoin() {
     setTimeout(() => {
         const result = Math.random() < 0.5 ? 'heads' : 'tails';
         coin.style.backgroundImage = result === 'heads' ? 
-            "url('https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/5_Rupees_Obverse.png/1024px-5_Rupees_Obverse.png')" : 
-            "url('https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/5_Rupees_Reverse.png/1024px-5_Rupees_Reverse.png')";
+            "url('https://upload.wikimedia.org/wikipedia/commons/e/e4/5_Rupees_Obverse.png')" : 
+            "url('https://upload.wikimedia.org/wikipedia/commons/5/59/5_Rupees_Reverse.png')";
         
         coin.classList.add('flip');
     }, 100);
